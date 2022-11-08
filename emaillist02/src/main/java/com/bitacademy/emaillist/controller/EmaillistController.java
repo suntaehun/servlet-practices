bee741eb00e482f7a84df93fa01dd12e3bd4f394
suntaehun <sth7862@naver.com>
2022-11-08 17:41:04 +0900

    guestbook02

--- emaillist02/src/main/java/com/bitacademy/emaillist/controller/EmaillistController.java
+++ emaillist02/src/main/java/com/bitacademy/emaillist/controller/EmaillistController.java
@@ -15,21 +15,39 @@ import com.bitacademy.emaillist.vo.EmaillistVo;
 public class EmaillistController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		
+
 		String action = request.getParameter("a");
-		if("list".equals(action)) {
+
+		if ("form".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/form.jsp");
+			rd.forward(request, response);
+		} else if ("add".equals(action)) {
+			String firstName = request.getParameter("fn");
+			String lastName = request.getParameter("ln");
+			String email = request.getParameter("email");
+
+			EmaillistVo vo = new EmaillistVo();
+			vo.setFirstName(firstName);
+			vo.setLastName(lastName);
+			vo.setEmail(email);
+
+			new EmaillistDao().insert(vo);
+
+			response.sendRedirect(request.getContextPath() + "/el");
+		} else {
 			List<EmaillistVo> list = new EmaillistDao().findAll();
-			
+
 			request.setAttribute("list", list);
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
 			rd.forward(request, response);
 		}
-		
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
